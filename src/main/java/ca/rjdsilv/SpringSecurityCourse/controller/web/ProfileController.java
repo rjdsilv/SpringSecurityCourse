471b4362033bc7f500fe72e2da8318dcf8836d3e
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2019-09-03 18:18:34 -0300

    Initial Commit

---
+++ src/main/java/ca/rjdsilv/SpringSecurityCourse/controller/web/ProfileController.java
@@ -0,0 +1,15 @@
+package ca.rjdsilv.SpringSecurityCourse.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("profile")
+public class ProfileController {
+
+    @GetMapping("index")
+    public String index(){
+        return "profile/index";
+    }
+}
---
+++ src/test/java/ca/rjdsilv/SpringSecurityCourse/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.rjdsilv.SpringSecurityCourse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityCourseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
